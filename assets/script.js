46e997f0bfc8fe2bd993a47807a9d1150833069f
Francisco Puma <francis_pancho12@hotmail.com>
2021-05-02 19:21:25 -0700

    added list of arrays for poll of options

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,22 @@
 // Assignment code here
 
+//List of Arays for each case 
+
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharacters = ["!", "#", "$", "%","&","(", ")", "*",
+                        "+", ",", "-", ".", "/", ":", ";", "<", 
+                        ">", "=", "?", "@", "[", "/", "]", "^", 
+                        "_", "{", "}", "~", ]; // based on the OWASP list of special characters
+var upperCaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L",
+                        "M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
+var lowerCaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o",
+                        "p","q","r","s","t","u","v","w","x","y","z",];
+
+
+
+
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
